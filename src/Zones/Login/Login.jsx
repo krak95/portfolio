7a65605aeb45eab7ce0200cf658f8b12abada44b
José Amorim <jpoa95@gmail.com>
2022-11-30 16:00:29 +0000

    css

--- src/Zones/Login/Login.jsx
+++ src/Zones/Login/Login.jsx
@@ -7,6 +7,8 @@ function Login() {
 const {authenticated,login,logout} = useContext(AuthContext);
 const [uname, setUname] = useState('');
 const [pwd, setPwd] = useState('');
+const [fname, setFname] = useState('');
+const [email, setEmail] = useState('');
 
 function handleLogout(e){
 e.preventDefault();
@@ -25,19 +27,25 @@ return (
 <div className="login-div">
 <div className='login-container'>
 <ol>
+<form onSubmit={handleForm}>
 <li><h2>Login</h2></li>
 <p>Username</p>
 <li><input type="text" onChange={(e)=>setUname(e.target.value)}/>   </li>
 <p>Password</p>
 <li><input type="password" onChange={(e)=>setPwd(e.target.value)}/></li>
-<li><button onClick={handleForm}>Submit</button></li>
+<li><button>Submit</button></li>
+</form>
 </ol>
 </div>
 <div className='signup-container'>
 <ol>
 <li><h2>Signup</h2></li>
+<p>Name</p>
+<li><input type="text" onChange={(e)=>setFname(e.target.value)}/>   </li>
+<p>Email</p>
+<li><input type="password" onChange={(e)=>setEmail(e.target.value)}/></li>
 <p>Username</p>
-<li><input type="text" onChange={(e)=>setUname(e.target.value)}/>   </li>
+<li><input type="password" onChange={(e)=>setUname(e.target.value)}/></li>
 <p>Password</p>
 <li><input type="password" onChange={(e)=>setPwd(e.target.value)}/></li>
 <li><button onClick={handleForm}>Submit</button></li>
