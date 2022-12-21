fa2d87880d4a46e314ece81cdcfb3a20901faa81
José Amorim <jpoa95@gmail.com>
2022-12-21 03:55:27 +0000

    chess pieces are moving?

--- src/Components/chess/board.jsx
+++ src/Components/chess/board.jsx
@@ -1,43 +1,46 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import $ from 'jquery'
 import Tile from "./tile"
+import { act } from "react-dom/test-utils";
 
 function Board(){
-
+const chessboardRef = useRef(null)
 const x = ['1','2','3','4','5','6','7','8']
 const y = ['1','2','3','4','5','6','7','8']
+
+console.log("chessboard",chessboardRef)
 
 const board = [];
 const pieces = [];
 
-pieces.push({piece:'tower', color:'black', x:0,y:0})
-pieces.push({piece:'tower', color:'black', x:0,y:7})
-pieces.push({piece:'bknight', color:'black', x:0,y:1})
-pieces.push({piece:'bknight', color:'black', x:0,y:6})
-pieces.push({piece:'bbish', color:'black', x:0,y:2})
-pieces.push({piece:'bbish', color:'black', x:0,y:5})
+pieces.push({piece:'bltower', color:'black', x:0,y:0})
+pieces.push({piece:'brtower', color:'black', x:0,y:7})
+pieces.push({piece:'blknight', color:'black', x:0,y:1})
+pieces.push({piece:'brknight', color:'black', x:0,y:6})
+pieces.push({piece:'blbish', color:'black', x:0,y:2})
+pieces.push({piece:'brbish', color:'black', x:0,y:5})
 pieces.push({piece:'bqueen', color:'black', x:0,y:3})
 pieces.push({piece:'bking', color:'black', x:0,y:4})
 
 for(let i = 0; i<x.length;i++){
-    pieces.push({piece:'pawn', color:'black', x:1,y:i})
+    pieces.push({piece:'bpawn'+[i], color:'black', x:1,y:i})
 }
 
-pieces.push({piece:'tower', color:'white', x:7,y:0})
-pieces.push({piece:'tower', color:'white', x:7,y:7})
-pieces.push({piece:'bknight', color:'white', x:7,y:1})
-pieces.push({piece:'bknight', color:'white', x:7,y:6})
-pieces.push({piece:'bbish', color:'white', x:7,y:2})
-pieces.push({piece:'bbish', color:'white', x:7,y:5})
-pieces.push({piece:'bqueen', color:'white', x:7,y:3})
-pieces.push({piece:'bking', color:'white', x:7,y:4})
+pieces.push({piece:'wltower', color:'white', x:7,y:0})
+pieces.push({piece:'wrtower', color:'white', x:7,y:7})
+pieces.push({piece:'wlknight', color:'white', x:7,y:1})
+pieces.push({piece:'wrknight', color:'white', x:7,y:6})
+pieces.push({piece:'wlbish', color:'white', x:7,y:2})
+pieces.push({piece:'wrbish', color:'white', x:7,y:5})
+pieces.push({piece:'wqueen', color:'white', x:7,y:3})
+pieces.push({piece:'wking', color:'white', x:7,y:4})
 
 for(let i = 0; i<x.length;i++){
-    pieces.push({piece:'pawn', color:'white', x:6,y:i})
+    pieces.push({piece:'wpawn'+[i], color:'white', x:6,y:i})
 }
 
 const [playstate, setPlaystate] = useState('pick')
-
+let activeP
 const pick = (e)=>{
     console.log('pick')
     e.stopPropagation()
@@ -52,26 +55,43 @@ const pick = (e)=>{
             color : e.target.getAttribute('color'),
         }
         localStorage.setItem('picked',JSON.stringify(picked))
-        if(picked.piece === 'pawn' && picked.color === 'black'){
+
+        if(picked.piece.includes('pawn') && picked.color.includes('black')){
             const xmove1 = parseInt(picked.x)+1;
             const xmove2 = parseInt(picked.x)+2;
             console.log('x',xmove1)
             console.log('x',xmove2)
             console.log('y',picked.y)
-        $('[tile-x='+picked.x+'][tile-y='+picked.y+']').addClass('possibleMoves')
-        $('[tile-x='+xmove1+'][tile-y='+picked.y+']').addClass('possibleMoves')
-        $('[tile-x='+xmove2+'][tile-y='+picked.y+']').addClass('possibleMoves')
-    }
-    setPlaystate('play')
+            $('[tile-x='+picked.x+'][tile-y='+picked.y+']').addClass('possibleMoves')
+            $('[tile-x='+xmove1+'][tile-y='+picked.y+']').addClass('possibleMoves')
+            $('[tile-x='+xmove2+'][tile-y='+picked.y+']').addClass('possibleMoves')
+        }
+        activeP = el
 }
 const play = (e) =>{
-    const elplay = e.target
-    const elstored = JSON.parse(localStorage.getItem('picked'))
-    $('[x='+elstored.x+'][y='+elstored.y+']').remove()
-    $('[tile-x='+elplay.x+'][tile-y='+elplay.y+']').append(<Tile onClick={playstate === 'pick' ? pick : play} x={elplay.tilex} y={elplay.tiley} piece={elstored.piece} number={elplay.x + elplay.y + 2} color={elstored.color}/>)
-    console.log(elstored.piece,elstored.color)
-    setPlaystate('pick')
+if(activeP){
+    const chessboard = chessboardRef.current
+    const stored = JSON.parse(localStorage.getItem('picked'))
+    const elpiece = document.querySelector('[piece='+stored.piece+']')
+    const mousex = e.clientX -30
+    const mousey = e.clientY -30
+    elpiece.style.left = `${mousex}px`
+    elpiece.style.top = `${mousey}px`
+    elpiece.style.position = 'absolute'
+    $('.chessboard div').removeClass('possibleMoves')
 }
+}
+
+const drop = (e) =>{
+    activeP=null
+}
+
+const mousepos = (e) =>{
+    const mousex = e.clientX
+    const mousey = e.clientY
+    console.log(mousex,mousey)
+}
+
     for (let i = 0; i < x.length; i++) {
         for(let j = 0; j < y.length; j++){
             const number = j+i+2;
@@ -84,13 +104,13 @@ const play = (e) =>{
                     color = el.color
                 }
             });
-            board.push(<Tile key={j+' '+i} onClick={playstate === 'pick' ? pick : play} x={i} y={j} piece={piece} number={number} color={color}/>)
+            board.push(<Tile key={j+' '+i} onMouseMove={play} onMouseUp={drop} onMouseDown={pick} x={i} y={j} piece={piece} number={number} color={color}/>)
         }
     }
 
     return(
         <>
-        <div className="chessboard">{board}</div>
+        <div className="chessboard" ref={chessboardRef}   >{board}</div>
         </>
     ) 
 }
--- src/Components/chess/tile.jsx
+++ src/Components/chess/tile.jsx
@@ -8,24 +8,24 @@ import bqueen from "./img/bqueen.png"
 import wqueen from "./img/wqueen.png"
 import bking from "./img/bking.png"
 
-function Tile({color,piece,number,onClick,x,y}){
+function Tile({color,piece,number,onMouseUp,onMouseDown,onMouseMove,x,y,refs}){
 
     if(number % 2 === 0 && piece !== undefined){
-        return <div tile-x={x} onClick={onClick} tile-y={y} className="tile black-tile">
-            <div color={color} onClick={onClick} x={x} y={y} piece={piece}>{color}<br></br>{piece}</div>
+        return <div tile-x={x} tile-y={y} className="tile black-tile">
+            <div color={color} refs={refs} onMouseUp={onMouseUp} onMouseMove={onMouseMove} onMouseDown={onMouseDown} x={x} y={y} piece={piece}>{color}<br></br>{piece}</div>
             </div>
     }
     if(number % 2 !== 0 && piece !== undefined){
-    return <div tile-x={x} onClick={onClick} tile-y={y}className="tile white-tile">
-        <div color={color} onClick={onClick} x={x} y={y} piece={piece}>{color}<br></br>{piece}</div>
+    return <div tile-x={x} tile-y={y}className="tile white-tile">
+        <div color={color} refs={refs} onMouseUp={onMouseUp} onMouseMove={onMouseMove} onMouseDown={onMouseDown} x={x} y={y} piece={piece}>{color}<br></br>{piece}</div>
         </div>
     }
     if(number % 2 === 0 && piece === undefined){
-        return <div tile-x={x} onClick={onClick} tile-y={y} className="tile black-tile">
+        return <div tile-x={x} tile-y={y} className="tile black-tile">
             </div>
     }
     if(number % 2 !== 0 && piece === undefined){
-        return <div tile-x={x} onClick={onClick} tile-y={y} className="tile white-tile">
+        return <div tile-x={x} tile-y={y} className="tile white-tile">
             </div>
     }
     
